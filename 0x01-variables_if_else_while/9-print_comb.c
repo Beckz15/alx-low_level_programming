df590e2043696a9f20aa502aa4e3bfe0c0108e56
Beckz15 <b.rabosiwana@gmail.com>
2023-02-20 19:36:24 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - main block
- * Description: prints all single digit number of base 10
- * starting from 0, followed by a new line.
- * Return: always 0 (Success)
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
-	for (ch = '0'; ch <= '9'; ch++)
+	int n;
+
+	for (n = 48; n < 58; n++)
 	{
-		putchar(ch);
-	}
-	for (ch = 'a'; ch <= 'f'; ch++)
-	{
-		putchar(ch);
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
